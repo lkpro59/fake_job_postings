68c45d932697085ce3839c2b211d5f4d257365be
lkpro59 <48743906+lkpro59@users.noreply.github.com>
2021-02-03 21:50:56 -0500

    Update Fake_Job_Posting_Classification_FINAL.R

--- Fake_Job_Posting_Classification_FINAL.R
+++ Fake_Job_Posting_Classification_FINAL.R
@@ -1,11 +1,3 @@
-#ECON_570 - Big Data - Final Project
-#This script is written to perform a classification on fake job posting dataset
-#The dataset is obtained from https://www.kaggle.com/shivamb/real-or-fake-fake-jobposting-prediction
-#This dataset contains 17,880 job descriptions out of which about 866 are fake. 
-#The data consists of both textual information and meta-information about the jobs. 
-#The dataset can be used to create classification models which can learn the job descriptions which are fraudulent. 
-#fake/scam postings are identified by the column "fraudulent" "1" for fake, 0 for real posting.
-
 #Modeling Pipeline
 #1. Analyze the data, understand the variables and decide which variables to use
 #2. Perform data cleaning and transformations on the variables of interest
